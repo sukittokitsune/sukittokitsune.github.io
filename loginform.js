36bec729e6167a95c28f00e37242bfaeb3eaac24
sukittokitsune <136924330+sukittokitsune@users.noreply.github.com>
2026-06-25 08:09:12 +0900

    Add files via upload

---
+++ loginform.js
@@ -0,0 +1,12 @@
+function validate() {
+    var name = document.getElementById("name").value;
+    var pass = document.getElementById("pass").value;
+    if (name == "aaa" && pass == "bbb") {
+        alert("Login successfully");
+		event.preventDefault();
+        window.location.href = "test admin.html"; // Redirecting to other page.
+		return false;
+    } else {
+		alert("Wrong password buddy, try again.");
+	
+	}}
